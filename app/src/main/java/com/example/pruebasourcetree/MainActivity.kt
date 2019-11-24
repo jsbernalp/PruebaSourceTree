bd88042d79a6373837620dfb76c33c59a161783a
Jonathan Bernal <jsbernalp@gmail.com>
2019-11-24 13:39:18 -0500

    prueba

--- app/src/main/java/com/example/pruebasourcetree/MainActivity.kt
+++ app/src/main/java/com/example/pruebasourcetree/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.pruebasourcetree
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Prueba("HOLA MUNDO")
+
+
+    }
+
+    fun Prueba(mensaje:String){
+
+        Toast.makeText(this,"Prueba $mensaje",Toast.LENGTH_LONG).show()
+
     }
 }
---
+++ app/src/main/java/com/example/pruebasourcetree/Pruebas.kt
@@ -0,0 +1,3 @@
+package com.example.pruebasourcetree
+
+class Pruebas (var name:String, var user:String, var age:Int)
